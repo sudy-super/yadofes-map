bdf6f6d30422f05555a3fdfc76afbe57e8df0894
sudy-super <sudy7info@gmail.com>
2025-05-28 15:11:15 +0900

    Apply Vercel Analytics

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+
 document.addEventListener('DOMContentLoaded', () => {
     const mapContainer = document.getElementById('mapContainer');
     const eventMapImage = document.getElementById('eventMap');
